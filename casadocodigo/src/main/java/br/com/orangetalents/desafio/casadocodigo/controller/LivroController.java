cbf020f38f8e59ca2c09da1ac2b0a061d7bd40d4
@TheoBaptista <theoalfonso78@gmail.com>
2021-03-19 10:47:58 -0300

    Criando o cotroller para detalhe livro

--- casadocodigo/src/main/java/br/com/orangetalents/desafio/casadocodigo/controller/LivroController.java
+++ casadocodigo/src/main/java/br/com/orangetalents/desafio/casadocodigo/controller/LivroController.java
@@ -6,9 +6,11 @@ import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.orangetalents.desafio.casadocodigo.controller.dto.LivroResponse;
@@ -52,6 +54,14 @@ public class LivroController {
 	@GetMapping
 	public List<LivroResponse> listaLivros(){
 		return LivroResponse.build(livroRepository.findAll());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> detalheLivro(@PathVariable("id") Long id){
+		
+		System.out.println(id);
+		
+		return ResponseEntity.ok().build();
 	}
 
 }
